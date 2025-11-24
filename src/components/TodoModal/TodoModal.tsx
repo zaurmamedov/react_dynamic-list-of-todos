6b4aec48b54ecbaa8b90e9c4555d557cfb3df024
Zaur <zaurmamedov339933@gmail.com>
2025-11-24 19:11:52 +0200

    Solution1

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,43 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todo: Todo;
+  selectedTodoId?: number;
+  onSelect?: (todo: Todo | null) => void;
+  loading: boolean;
+};
+
+export const TodoModal: React.FC<Props> = ({
+  todo,
+  selectedTodoId,
+  onSelect = () => {},
+  loading,
+}) => {
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (!loading) {
+      timer = setTimeout(() => setShowLoader(false), 500); // затримка
+    }
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [loading]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
-
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
-            </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-            </p>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <div
+            className="modal-card-title has-text-weight-medium"
+            data-cy="modal-header"
+          >
+            Todo #{selectedTodoId}
           </div>
+
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            type="button"
+            className="delete"
+            data-cy="modal-close"
+            onClick={() => onSelect(null)}
+          />
+        </header>
+
+        <div className="modal-card-body">
+          {showLoader ? (
+            <Loader />
+          ) : (
+            <>
+              <p className="block" data-cy="modal-title">
+                {todo.title}
+              </p>
+              <p className="block" data-cy="modal-user">
+                {todo.completed ? (
+                  <strong className="has-text-success">Done</strong>
+                ) : (
+                  <strong className="has-text-danger">Planned</strong>
+                )}
+
+                {' by '}
+
+                {todo.user && (
+                  <a href={`mailto:${todo.user.email}`}>{todo.user.name}</a>
+                )}
+              </p>
+            </>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
